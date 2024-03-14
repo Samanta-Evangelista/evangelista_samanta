22bedd01260edf2ab06a8d6396eaf2c0adb52f64
samanta evangelista <samidanieva@gmail.com>
2024-03-14 01:59:39 -0300

    Modificaciones en productManager

--- desarrollo_backend/primera_preentrega/src/productManager.js
+++ desarrollo_backend/primera_preentrega/src/productManager.js
@@ -7,48 +7,73 @@ export class ProductManager {
     constructor() {
         this.path = 'products.json';
         this.products = []
-
-        // parametros del producto
-        addProducts = async ({ title, description, price, thumbnail, code, stock, status, category }) => {
-
-            //se genera un id unico y aleatorio
-            const id = uuidv4()
-
-            //le agregamos el id al producto
-            let newProduct = {
-                id, title, description, price, thumbnail, code, stock, status, category
-            }
-
-            this.products=await this.getProducts();
-            this.products.push(newProduct);
-
-            //guardamos en el archivo JSON todo el array, que estaba vacio, con el nuevo producto.
-            await fs.writeFile(this.path, JSON.stringify(this.products));
-
-            return newProduct;
-        }
-        getProducts = async () => {
-            const response = await fs.readFile(this.path, 'utf-8');
-            const responseJSON=JSON.parse(response)
-
-            return responseJSON;
-
-        }
-
-        getProductsById= async (id) => {
-const response=this.getProducts();
-
-const product= response.find(product=> product.id === id)
-
-if(product){
-    return product
-} else {
-    console.log('Producto no encontrado');
-}
-        }
-
-        
     }
 
+    
+    //agrgar producto
+    addProducts = async ({ title, description, price, thumbnail, code, stock, status, category }) => {
 
+        const id = uuidv4()
+
+        let newProduct = {
+            id, title, description, price, thumbnail, code, stock, status, category
+        }
+
+        this.products = await this.getProducts();
+        this.products.push(newProduct);
+
+        await fs.writeFile(this.path, JSON.stringify(this.products));
+
+        return newProduct;
+    }
+
+    //Obtener todos los productos
+    getProducts = async () => {
+        const response = await fs.readFile(this.path, 'utf-8');
+        const responseJSON = JSON.parse(response)
+
+        return responseJSON;
+
+    }
+
+    //Obtener un producto especifico.
+    getProductsById = async (id) => {
+        const response = this.getProducts();
+
+        const product = response.find(product => product.id === id)
+
+        if (product) {
+            return product
+        } else {
+            console.log('Producto no encontrado');
+        }
+    }
+
+    //actualizar un producto especifico
+    updateProduct = async (id, { ...data }) => {
+        const response = this.getProducts()
+        const index = response.findIndex(product => product.id === id)
+
+        if (index !== -1) {
+            response[index] = { id, ...data }
+            await fs.writeFile(this.path, JSON.stringify(response))
+            return response[index]
+        } else {
+            console.log('Producto no encontrado');
+        }
+    }
+
+    //eliminar producto especifico. 
+    deleteProducts = async (id) => {
+        const products = this.getProducts()
+        const index = products.findIndex(product => product.id === id)
+
+        if (index !== -1) {
+            products.splice(index, 1)
+            await fs.writeFile(this.path, JSON.stringify(products))
+        } else{
+            console.log('Producto no encotrado');
+        }
+
+    }
 }
