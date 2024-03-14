b1a45b46614f2a760439e7bb790be377bbbac26f
samanta evangelista <samidanieva@gmail.com>
2024-03-14 02:58:29 -0300

    Modificaciones en el index.js

--- desarrollo_backend/primera_preentrega/src/index.js
+++ desarrollo_backend/primera_preentrega/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ProductManager } from './productManager.js';
 import { productsRouter } from './routes/products.router.js';
-import e from 'express';
+
 
 const PORT = 8080;
 
@@ -9,7 +9,7 @@ const app = express();
 
 export const productManager = new ProductManager;
 
-app.use('/products', productsRouter)
+app.use('/api/products', productsRouter)
 
 app.listen(PORT, (req, res) => {
     console.log('Servidor escuchando en el puerto ${PORT}');
