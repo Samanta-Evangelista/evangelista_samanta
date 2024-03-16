29044afb6fdd52b8df990652ab6525a817d96050
samanta evangelista <samidanieva@gmail.com>
2024-03-16 10:45:03 -0300

    Se crea cartManager

---
+++ desarrollo_backend/primera_preentrega/src/cartManager.js
@@ -0,0 +1,53 @@
+import { promises as fs} from 'fs'
+import { v4 as uuidv4 } from 'uuidv4'
+
+export class CartManager {
+
+    constructor(){
+        this.path = 'cart.json';
+        this.carts = [];
+    }
+}
+
+getCarts = async () => {
+const response = await fs.readFile(this.path, 'utf8')
+const responseJSON = JSON.parse(response)
+return responseJSON
+}
+
+getCartProducts = async (id) =>{
+    const carts = await this.getCarts()
+
+    const cart = carts.find (cart0>cart.id === id);
+
+    if(cart){
+        return cart.products
+    }else{
+        console.log(`Carrito no encontrado`);
+    }
+
+}
+
+newCart = async () =>{
+    const id = uuidv4()
+
+    const newCart = {id, products: []}
+
+    this.carts = await this.getCarts()
+    this.carts.push(newCart)
+
+    await fs.writeFile(this.path, JSON.stringify(this.carts))
+    return newCart;
+}
+
+addProductToCart = async (cart_id, product_id) =>{
+const carts = await this.getCarts()
+const index = carts.findIndex(cart => cart.id === cart_id)
+
+if(index != -1){
+const cartProducts = await this.getCartProducts(cart_id )
+const existingProductIndex = cartProducts.findIndex(product=> product_id )
+}
+
+
+}
